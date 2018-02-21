5d6a6fa13547c0dc84fd0481ba33fc9ca85b0e50
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-02-21 16:47:31 +0530

    5 multiple

---
+++ 5 multiples-21-2-18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+int n,i,a;
+scanf ("%d",&n);
+printf ("first number\n%d",n);
+printf ("\final number\n");
+for(i=1;i<=5;i++)
+   {
+	a=n*i;
+	printf("%d\t",a);
+   }
+
+	return 0;
+}
